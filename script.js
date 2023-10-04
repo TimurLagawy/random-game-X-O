7c72d5865b4eb2838829f97db73dfc0a0ddbd39d
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-04 15:11:30 +0200

    feat: add limitation to click

--- script.js
+++ script.js
@@ -10,74 +10,101 @@ let pole9 = document.querySelector("#pole9");
 let count = 0;
 
 pole1.addEventListener("click", () => {
-  if (count % 2 == 1) {
-    pole1.innerHTML = "O";
+  if (pole1.innerHTML == "") {
+    if (count % 2 == 1) {
+      pole1.innerHTML = "O";
+    } else {
+      pole1.innerHTML = "X";
+    }
+    count++;
   } else {
-    pole1.innerHTML = "X";
   }
-  count++;
 });
 pole2.addEventListener("click", () => {
-  if (count % 2 === 1) {
-    pole2.innerHTML = "O";
+  if (pole2.innerHTML == "") {
+    if (count % 2 == 1) {
+      pole2.innerHTML = "O";
+    } else {
+      pole2.innerHTML = "X";
+    }
+    count++;
   } else {
-    pole2.innerHTML = "X";
   }
-  count++;
 });
 pole3.addEventListener("click", () => {
-  if (count % 2 === 1) {
-    pole3.innerHTML = "O";
+  if (pole3.innerHTML == "") {
+    if (count % 2 == 1) {
+      pole3.innerHTML = "O";
+    } else {
+      pole3.innerHTML = "X";
+    }
+    count++;
   } else {
-    pole3.innerHTML = "X";
   }
-  count++;
 });
 pole4.addEventListener("click", () => {
-  if (count % 2 == 1) {
-    pole4.innerHTML = "O";
+  if (pole4.innerHTML == "") {
+    if (count % 2 == 1) {
+      pole4.innerHTML = "O";
+    } else {
+      pole4.innerHTML = "X";
+    }
+    count++;
   } else {
-    pole4.innerHTML = "X";
   }
-  count++;
 });
 pole5.addEventListener("click", () => {
-  if (count % 2 == 1) {
-    pole5.innerHTML = "O";
+  if (pole5.innerHTML == "") {
+    if (count % 2 == 1) {
+      pole5.innerHTML = "O";
+    } else {
+      pole5.innerHTML = "X";
+    }
+    count++;
   } else {
-    pole5.innerHTML = "X";
   }
-  count++;
 });
 pole6.addEventListener("click", () => {
-  if (count % 2 == 1) {
-    pole6.innerHTML = "O";
+  if (pole6.innerHTML == "") {
+    if (count % 2 == 1) {
+      pole6.innerHTML = "O";
+    } else {
+      pole6.innerHTML = "X";
+    }
+    count++;
   } else {
-    pole6.innerHTML = "X";
   }
-  count++;
 });
 pole7.addEventListener("click", () => {
-  if (count % 2 == 1) {
-    pole7.innerHTML = "O";
+  if (pole7.innerHTML == "") {
+    if (count % 2 == 1) {
+      pole7.innerHTML = "O";
+    } else {
+      pole7.innerHTML = "X";
+    }
+    count++;
   } else {
-    pole7.innerHTML = "X";
   }
-  count++;
 });
 pole8.addEventListener("click", () => {
-  if (count % 2 == 1) {
-    pole8.innerHTML = "O";
+  if (pole8.innerHTML == "") {
+    if (count % 2 == 1) {
+      pole8.innerHTML = "O";
+    } else {
+      pole8.innerHTML = "X";
+    }
+    count++;
   } else {
-    pole8.innerHTML = "X";
   }
-  count++;
 });
 pole9.addEventListener("click", () => {
-  if (count % 2 == 1) {
-    pole9.innerHTML = "O";
+  if (pole9.innerHTML == "") {
+    if (count % 2 == 1) {
+      pole9.innerHTML = "O";
+    } else {
+      pole9.innerHTML = "X";
+    }
+    count++;
   } else {
-    pole9.innerHTML = "X";
   }
-  count++;
 });
