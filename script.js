9826ecfba8debddffb98acba070084dcffb5a7e5
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-04 15:03:41 +0200

    feat: add logic to aplication

---
+++ script.js
@@ -0,0 +1,83 @@
+let pole1 = document.querySelector("#pole1");
+let pole2 = document.querySelector("#pole2");
+let pole3 = document.querySelector("#pole3");
+let pole4 = document.querySelector("#pole4");
+let pole5 = document.querySelector("#pole5");
+let pole6 = document.querySelector("#pole6");
+let pole7 = document.querySelector("#pole7");
+let pole8 = document.querySelector("#pole8");
+let pole9 = document.querySelector("#pole9");
+let count = 0;
+
+pole1.addEventListener("click", () => {
+  if (count % 2 == 1) {
+    pole1.innerHTML = "O";
+  } else {
+    pole1.innerHTML = "X";
+  }
+  count++;
+});
+pole2.addEventListener("click", () => {
+  if (count % 2 === 1) {
+    pole2.innerHTML = "O";
+  } else {
+    pole2.innerHTML = "X";
+  }
+  count++;
+});
+pole3.addEventListener("click", () => {
+  if (count % 2 === 1) {
+    pole3.innerHTML = "O";
+  } else {
+    pole3.innerHTML = "X";
+  }
+  count++;
+});
+pole4.addEventListener("click", () => {
+  if (count % 2 == 1) {
+    pole4.innerHTML = "O";
+  } else {
+    pole4.innerHTML = "X";
+  }
+  count++;
+});
+pole5.addEventListener("click", () => {
+  if (count % 2 == 1) {
+    pole5.innerHTML = "O";
+  } else {
+    pole5.innerHTML = "X";
+  }
+  count++;
+});
+pole6.addEventListener("click", () => {
+  if (count % 2 == 1) {
+    pole6.innerHTML = "O";
+  } else {
+    pole6.innerHTML = "X";
+  }
+  count++;
+});
+pole7.addEventListener("click", () => {
+  if (count % 2 == 1) {
+    pole7.innerHTML = "O";
+  } else {
+    pole7.innerHTML = "X";
+  }
+  count++;
+});
+pole8.addEventListener("click", () => {
+  if (count % 2 == 1) {
+    pole8.innerHTML = "O";
+  } else {
+    pole8.innerHTML = "X";
+  }
+  count++;
+});
+pole9.addEventListener("click", () => {
+  if (count % 2 == 1) {
+    pole9.innerHTML = "O";
+  } else {
+    pole9.innerHTML = "X";
+  }
+  count++;
+});
